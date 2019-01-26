391f15a35be049351a0632310eea3483c0580c29
kqian5 <kycqian1005@gmail.com>
2019-01-26 14:33:07 -0500

    initial

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+import requests
 from flask import render_template
+from flask import request
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/results')
+def results():
+    params = request.query_string
+    #access api and search the params
+    resp = requests.get()
+    return render_template('results.html')
+
+@app.route('/search')
+def results():
+    return render_template('search.html')
